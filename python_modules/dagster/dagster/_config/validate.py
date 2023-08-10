3cf2b3899653456ffeb0fe97ccf255b4a905de56
benpankow <ben@elementl.com>
2023-08-10 10:24:30 -0500

    get args

--- python_modules/dagster/dagster/_config/validate.py
+++ python_modules/dagster/dagster/_config/validate.py
@@ -1,5 +1,7 @@
 from typing import Any, Dict, List, Mapping, Optional, Sequence, Set, TypeVar, cast
 
+from typing_extensions import get_args
+
 import dagster._check as check
 from dagster._config.field_utils import DagsterEnvVar
 from dagster._utils import ensure_single_item
@@ -110,7 +112,7 @@ def _validate_config(context: ValidationContext, config_value: object) -> Evalua
         if not is_config_scalar_valid(context.config_type_snap, config_value):
             return EvaluateValueResult.for_error(create_scalar_error(context, config_value))
         # If user passes an EnvVar or IntEnvVar to a non-structured run config dictionary, throw explicit error
-        if not context.allow_envvar and isinstance(config_value, DagsterEnvVar):
+        if not context.allow_envvar and isinstance(config_value, get_args(DagsterEnvVar)):
             return EvaluateValueResult.for_error(
                 create_pydantic_env_var_error(context, config_value)
             )
